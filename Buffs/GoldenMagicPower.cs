b81311af6c864a96df4c8cb5f4831479c324cfb9
ThEnderYoshi <ranesword@gmail.com>
2022-06-19 16:31:00 -0300

    Added Golden Magic Power Potion

---
+++ Buffs/GoldenMagicPower.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Buffs
+{
+    internal class GoldenMagicPower : GoldenBuff
+    {
+        public override int OverwriteBuff
+        {
+            get { return BuffID.MagicPower; }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Magic Power++");
+            Description.SetDefault("40% increased magic damage");
+        }
+
+        public override void SafeUpdate(Player player, ref int buffIndex)
+        {
+            player.GetDamage(DamageClass.Magic) *= 1.4f;
+        }
+    }
+}
---
+++ Items/Potions/Buff/GoldenMagicPowerPotion.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GoldenPotions.Buffs;
+
+namespace GoldenPotions.Items.Potions.Buff
+{
+    internal class GoldenMagicPowerPotion : GoldenPotion
+    {
+        public override int NormalPotion
+        {
+            get { return ItemID.MagicPowerPotion; }
+        }
+
+        public override void SafeStaticDefaults()
+        {
+            Tooltip.SetDefault("40% increased magic damage");
+        }
+
+        public override void SafeDefaults()
+        {
+            Item.width = 32;
+            Item.height = 34;
+
+            Item.buffType = ModContent.BuffType<GoldenMagicPower>();
+            Item.buffTime = 14400; // 4 minutes
+        }
+    }
+}
