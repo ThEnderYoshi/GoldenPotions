b35d195606f1049e6d661689e476693623739f91
ThEnderYoshi <ranesword@gmail.com>
2022-06-19 12:43:48 -0300

    Added Golden Lifeforce Potion

---
+++ Buffs/GoldenLifeforce.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace GoldenPotions.Buffs
+{
+    internal class GoldenLifeforce : GoldenBuff
+    {
+        public override int OverwriteBuff
+        {
+            get { return BuffID.Lifeforce; }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Lifeforce++");
+            Description.SetDefault("40% increased max life");
+        }
+
+        public override void SafeUpdate(Player player, ref int buffIndex)
+        {
+            player.statLifeMax2 += player.statLifeMax / 5 / 20 * 20 * 2;
+            // Why are there 2 max lives?
+        }
+    }
+}
---
+++ Items/Potions/Buff/GoldenLifeforcePotion.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GoldenPotions.Buffs;
+
+namespace GoldenPotions.Items.Potions.Buff
+{
+    internal class GoldenLifeforcePotion : GoldenPotion
+    {
+        public override int NormalPotion
+        {
+            get { return ItemID.LifeforcePotion; }
+        }
+
+        public override void SafeStaticDefaults()
+        {
+            Tooltip.SetDefault("Increases max life by 40%");
+        }
+
+        public override void SafeDefaults()
+        {
+            Item.width = 28;
+            Item.height = 34;
+
+            Item.buffType = ModContent.BuffType<GoldenLifeforce>();
+            Item.buffTime = 28800; // 8 minutes
+        }
+    }
+}
