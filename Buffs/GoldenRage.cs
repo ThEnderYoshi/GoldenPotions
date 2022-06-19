31968547426f6c716f05e11ab3b9bef92679f448
ThEnderYoshi <ranesword@gmail.com>
2022-06-19 20:21:16 -0300

    Added Golden Rage Potion

---
+++ Buffs/GoldenRage.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Buffs
+{
+    internal class GoldenRage : GoldenBuff
+    {
+        public override int OverwriteBuff
+        {
+            get { return BuffID.Rage; }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Rage++");
+            Description.SetDefault("20% increased critical chance");
+        }
+
+        public override void SafeUpdate(Player player, ref int buffIndex)
+        {
+            player.GetCritChance(DamageClass.Generic) *= 1.2f;
+        }
+    }
+}
---
+++ Items/Potions/Buff/GoldenRagePotion.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using GoldenPotions.Buffs;
+
+namespace GoldenPotions.Items.Potions.Buff
+{
+    internal class GoldenRagePotion : GoldenPotion
+    {
+        public override int NormalPotion
+        {
+            get { return ItemID.RagePotion; }
+        }
+
+        public override void SafeStaticDefaults()
+        {
+            Tooltip.SetDefault("Increases critical chance by 20%");
+        }
+
+        public override void SafeDefaults()
+        {
+            Item.width = 32;
+            Item.height = 34;
+
+            Item.buffType = ModContent.BuffType<GoldenRage>();
+            Item.buffTime = 14400; // 4 minutes
+        }
+    }
+}
