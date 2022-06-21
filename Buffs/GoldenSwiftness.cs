23a9bc56bc1ccc871d84771a30a3555f8b52e49d
ThEnderYoshi <ranesword@gmail.com>
2022-06-21 15:43:17 -0300

    Added Golden Swiftness Potion

---
+++ Buffs/GoldenSwiftness.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace GoldenPotions.Buffs
+{
+    internal class GoldenSwiftness : GoldenBuff
+    {
+        public override int OverwriteBuff
+        {
+            get { return BuffID.Swiftness; }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Swiftness++");
+            Description.SetDefault("50% increased movement speed");
+        }
+
+        public override void SafeUpdate(Player player, ref int buffIndex)
+        {
+            player.moveSpeed += 0.5f;
+        }
+    }
+}
---
+++ Items/Potions/Buff/GoldenSwiftnessPotion.cs
@@ -0,0 +1,29 @@
+﻿using GoldenPotions.Buffs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Items.Potions.Buff
+{
+    internal class GoldenSwiftnessPotion : GoldenPotion
+    {
+        public override int NormalPotion
+        {
+            get { return ItemID.SwiftnessPotion; }
+        }
+
+        public override void SafeStaticDefaults()
+        {
+            Tooltip.SetDefault("50% increased movement speed");
+        }
+
+        public override void SafeDefaults()
+        {
+            Item.width = 44;
+            Item.height = 32;
+
+            Item.buffType = ModContent.BuffType<GoldenSwiftness>();
+            Item.buffTime = 28800; // 8 minutes
+        }
+    }
+}
