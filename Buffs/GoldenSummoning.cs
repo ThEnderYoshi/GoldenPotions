a5f4fccd8d80eb116201568fdd3d21d27d3deec6
ThEnderYoshi <ranesword@gmail.com>
2022-06-21 01:34:25 -0300

    Added Golden Summoning Potion

---
+++ Buffs/GoldenSummoning.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+
+namespace GoldenPotions.Buffs
+{
+    internal class GoldenSummoning : GoldenBuff
+    {
+        public override int OverwriteBuff
+        {
+            get { return BuffID.Summoning; }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Summoning++");
+            Description.SetDefault("Increased max number of minions");
+        }
+
+        public override void SafeUpdate(Player player, ref int buffIndex)
+        {
+            player.maxMinions += 2;
+        }
+    }
+}
---
+++ Items/Potions/Buff/GoldenSummoningPotion.cs
@@ -0,0 +1,29 @@
+﻿using GoldenPotions.Buffs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Items.Potions.Buff
+{
+    internal class GoldenSummoningPotion : GoldenPotion
+    {
+        public override int NormalPotion
+        {
+            get { return ItemID.SummoningPotion; }
+        }
+
+        public override void SafeStaticDefaults()
+        {
+            Tooltip.SetDefault("Increases your max number of minions by 2");
+        }
+
+        public override void SafeDefaults()
+        {
+            Item.width = 36;
+            Item.height = 34;
+
+            Item.buffType = ModContent.BuffType<GoldenSummoning>();
+            Item.buffTime = 28800; // 8 minutes
+        }
+    }
+}
