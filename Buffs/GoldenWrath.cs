85219a6f7f726daa67dbc7bfe61acce86b60a411
ThEnderYoshi <ranesword@gmail.com>
2022-06-21 20:38:41 -0300

    Added Golden Rage Potion

---
+++ Buffs/GoldenWrath.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Buffs
+{
+    internal class GoldenWrath : GoldenBuff
+    {
+        public override int OverwriteBuff
+        {
+            get { return BuffID.Titan; }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Wrath++");
+            Description.SetDefault("20% increased damage");
+        }
+
+        public override void SafeUpdate(Player player, ref int buffIndex)
+        {
+            player.GetDamage(DamageClass.Generic) *= 1.2f;
+        }
+    }
+}
---
+++ Items/Potions/Buff/GoldenWrathPotion.cs
@@ -0,0 +1,29 @@
+﻿using GoldenPotions.Buffs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Items.Potions.Buff
+{
+    internal class GoldenWrathPotion : GoldenPotion
+    {
+        public override int NormalPotion
+        {
+            get { return ItemID.WrathPotion; }
+        }
+
+        public override void SafeStaticDefaults()
+        {
+            Tooltip.SetDefault("Increases damage by 20%");
+        }
+
+        public override void SafeDefaults()
+        {
+            Item.width = 36;
+            Item.height = 38;
+
+            Item.buffType = ModContent.BuffType<GoldenWrath>();
+            Item.buffTime = 14400; // 4 minutes
+        }
+    }
+}
