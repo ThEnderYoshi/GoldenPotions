5c063ed77a79e88d264afb5e9ee42728ab1006e9
ThEnderYoshi <ranesword@gmail.com>
2022-06-21 16:25:11 -0300

    Added Golden Titan Potion

---
+++ Buffs/GoldenTitan.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Buffs
+{
+    internal class GoldenTitan : GoldenBuff
+    {
+        public override int OverwriteBuff
+        {
+            get { return BuffID.Titan; }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Titan++");
+            Description.SetDefault("Greatly increased knockback");
+        }
+
+        public override void SafeUpdate(Player player, ref int buffIndex)
+        {
+            player.GetKnockback(DamageClass.Generic) *= 2f; // +100% knockback
+        }
+    }
+}
---
+++ Items/Potions/Buff/GoldenTitanPotion.cs
@@ -0,0 +1,29 @@
+﻿using GoldenPotions.Buffs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GoldenPotions.Items.Potions.Buff
+{
+    internal class GoldenTitanPotion : GoldenPotion
+    {
+        public override int NormalPotion
+        {
+            get { return ItemID.TitanPotion; }
+        }
+
+        public override void SafeStaticDefaults()
+        {
+            Tooltip.SetDefault("Greatly increases knockback");
+        }
+
+        public override void SafeDefaults()
+        {
+            Item.width = 44;
+            Item.height = 30;
+
+            Item.buffType = ModContent.BuffType<GoldenTitan>();
+            Item.buffTime = 28800; // 8 minutes
+        }
+    }
+}
